89adde78c661a0796b3009e350680fbede6b409f
Fernando Espinosa Victoriano <fespinosa@fespinosa.nt.core>
2017-12-06 15:53:16 -0400

    Comment code

--- js/design.js
+++ js/design.js
@@ -1,59 +1,81 @@
 $(function() {
-	$("#submit").click(doSubmit);
+  $("#submit").click(doSubmit);
 
-	function doSubmit() {
-		if (gridExits() && gridSizeChanged()) {
-			resetGrid();
-		}
-		if (!gridExits()) {
-			makeGrid();
-		}
-	}
+  function doSubmit() {
+    if (gridExits() && gridSizeChanged()) {
+      resetGrid();
+    }
+    if (!gridExits()) {
+      makeGrid();
+    }
+  }
 
-	function gridExits() {
-		return $("#grid").length;
-	}
+  /**
+   * @description Checks if the grid already exits or not
+   * @returns {boolean} true or false
+   */
+  function gridExits() {
+    return $("#grid").length;
+  }
 
-	function gridSizeChanged() {
-		return $("#grid").length != gridSize();
-	}
+  /**
+   * @description Checks if the grid size isn't equal to the total inputs values
+   * @returns {boolean} true or false
+   */
+  function gridSizeChanged() {
+    return $("#grid").length != gridSize();
+  }
 
-	function gridSize() {
-		let columnCount = $("#grid-height").val();
-		let rowCount = $("#grid-width").val();
-		let size = columnCount * rowCount;
-		return size;
-	}
+  /**
+   * @description Calculate the grid size
+   * @returns {number} size of the grid
+   */
+  function gridSize() {
+    let columnCount = $("#grid-height").val();
+    let rowCount = $("#grid-width").val();
+    let size = columnCount * rowCount;
+    return size;
+  }
 
-	function resetGrid() {
-		$("#grid").remove();
-		makeGrid();
-	}
+  /**
+  * @description Deletes the grid and creates a new one
+  */
+  function resetGrid() {
+    $("#grid").remove();
+    makeGrid();
+  }
 
-	function makeGrid() {
-		let grid = $("<table cellpadding='0' cellspacing='0' id='grid'></table>");
-		let columnCount = $("#grid-height").val();
-		let rowCount = $("#grid-width").val();
-		for (var i = 0; i < columnCount; i++) {
-			var row = $("<tr></tr>").addClass("column");
-			for (var j = 0; j < rowCount; j++) {
-				let cell = $("<td></td>").addClass("cell").click(cellClickedListener);
-				row.append(cell);
-			}
-			grid.append(row);
-		}
-		$(".page-wrap").append(grid);
-	}
+  /**
+  * @description Creates a grid using the inputs values
+  */
+  function makeGrid() {
+    let grid = $("<table cellpadding='0' cellspacing='0' id='grid'></table>");
+    let columnCount = $("#grid-height").val();
+    let rowCount = $("#grid-width").val();
+    for (var i = 0; i < columnCount; i++) {
+      var row = $("<tr></tr>").addClass("column");
+      for (var j = 0; j < rowCount; j++) {
+        let cell = $("<td></td>").addClass("cell").click(cellClickedListener);
+        row.append(cell);
+      }
+      grid.append(row);
+    }
+    $(".page-wrap").append(grid);
+  }
 
-	function cellClickedListener(event) {
-		let cell = event.currentTarget;
-		cell.style.backgroundColor = getPickedColor();
-	}
+  function cellClickedListener(event) {
+    let cell = event.currentTarget;
+    cell.style.backgroundColor = getPickedColor();
+  }
 
-	function getPickedColor() {
-		let colorPicker = document.querySelector(".jscolor");
-		let bckColor = colorPicker.getAttribute("style").split(";")[1];
-		let color = bckColor.substring(bckColor.indexOf("rgb"), bckColor.length);
-		return color;
-	}
+  /**
+  * @description gets the selected color
+  * @returns picked color
+  */
+  function getPickedColor() {
+    let colorPicker = document.querySelector(".jscolor");
+    let bckColor = colorPicker.getAttribute("style").split(";")[1];
+    let color = bckColor.substring(bckColor.indexOf("rgb"), bckColor.length);
+    return color;
+  }
 });
