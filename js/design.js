23b8d3f8db73a58e31a18ed4cb2ebecff0db0e00
Fernando Espinosa Victoriano <fespinosa@fespinosa.nt.core>
2017-12-06 10:31:42 -0400

    Beautify code

--- js/design.js
+++ js/design.js
@@ -1,61 +1,59 @@
 $(function() {
+	$("#submit").click(doSubmit);
 
-  $("#submit").click(doSubmit);
+	function doSubmit() {
+		if (gridExits() && gridSizeChanged()) {
+			resetGrid();
+		}
+		if (!gridExits()) {
+			makeGrid();
+		}
+	}
 
-  function doSubmit() {
-    if (gridExits() && gridSizeChanged()) {
-      resetGrid();
-    }
-    if (!gridExits()) {
-      makeGrid();
-    }
-  }
+	function gridExits() {
+		return $("#grid").length;
+	}
 
-  function gridExits() {
-    return $("#grid").length;
-  }
+	function gridSizeChanged() {
+		return $("#grid").length != gridSize();
+	}
 
-  function gridSizeChanged() {
-    return $("#grid").length != gridSize();
-  }
+	function gridSize() {
+		let columnCount = $("#grid-height").val();
+		let rowCount = $("#grid-width").val();
+		let size = columnCount * rowCount;
+		return size;
+	}
 
-  function gridSize() {
-    let columnCount = $("#grid-height").val();
-    let rowCount = $("#grid-width").val();
-    let size = columnCount * rowCount;
-    return size;
-  }
+	function resetGrid() {
+		$("#grid").remove();
+		makeGrid();
+	}
 
-  function resetGrid() {
-    $("#grid").remove();
-    makeGrid();
-  }
+	function makeGrid() {
+		let grid = $("<table cellpadding='0' cellspacing='0' id='grid'></table>");
+		let columnCount = $("#grid-height").val();
+		let rowCount = $("#grid-width").val();
+		for (var i = 0; i < columnCount; i++) {
+			var row = $("<tr></tr>").addClass("column");
+			for (var j = 0; j < rowCount; j++) {
+				let cell = $("<td></td>").addClass("cell").click(cellClickedListener);
+				row.append(cell);
+			}
+			grid.append(row);
+		}
+		$(".page-wrap").append(grid);
+	}
 
-  function makeGrid() {
-    let grid = $("<table cellpadding='0' cellspacing='0' id='grid'></table>");
-    let columnCount = $("#grid-height").val();
-    let rowCount = $("#grid-width").val();
-    for (var i = 0; i < columnCount; i++) {
-      var row = $("<tr></tr>").addClass("column");
-      for (var j = 0; j < rowCount; j++) {
-        let cell = $("<td></td>").addClass("cell").click(cellClickedListener);
-        row.append(cell);
-      }
-      grid.append(row);
-    }
-    $(".page-wrap").append(grid);
-  }
+	function cellClickedListener(event) {
+		let cell = event.currentTarget;
+		cell.style.backgroundColor = getPickedColor();
+	}
 
-  function cellClickedListener(event) {
-    let cell = event.currentTarget;
-    cell.style.backgroundColor = getPickedColor();
-  }
-
-  function getPickedColor() {
-    let colorPicker = document.querySelector(".jscolor");
-    let bckColor = colorPicker.getAttribute("style").split(";")[1];
-    let color = bckColor.substring(bckColor.indexOf("rgb"), bckColor.length);
-    return color;
-  }
-
+	function getPickedColor() {
+		let colorPicker = document.querySelector(".jscolor");
+		let bckColor = colorPicker.getAttribute("style").split(";")[1];
+		let color = bckColor.substring(bckColor.indexOf("rgb"), bckColor.length);
+		return color;
+	}
 });
